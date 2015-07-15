34f8445070ab618f8228a0e46f1ef8123daf1230
Martin Cajthaml <martin.cajthaml@symbiodigital.com>
2015-07-15 11:21:07 +0200

    preg_match

--- Twig/OrangeGate4Extension.php
+++ Twig/OrangeGate4Extension.php
@@ -8,6 +8,7 @@ class OrangeGate4Extension extends \Twig_Extension
     {
         return array(
             new \Twig_SimpleFilter('preg_replace', array($this, 'pregReplaceFilter')),
+            new \Twig_SimpleFilter('preg_match', array($this, 'pregMatchFilter')),
         );
     }
 
@@ -18,6 +19,18 @@ class OrangeGate4Extension extends \Twig_Extension
         }
         else {
             return preg_replace($pattern, $replacement, $subject, $limit);
+        }
+    }
+
+    public function pregMatchFilter($subject, $pattern, $replacement='', $limit=-1)
+    {
+        if (!isset($subject)) {
+            return null;
+        }
+        else {
+            preg_match($pattern, $subject, $matches);
+
+            return $matches;
         }
     }
 
