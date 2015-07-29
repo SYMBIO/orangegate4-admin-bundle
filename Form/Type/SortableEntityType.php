91849afd99fd0316ceb636c4b7cefa57c277be54
Martin Cajthaml <martin.cajthaml@symbiodigital.com>
2015-07-29 07:25:10 +0200

    sortable entity

---
+++ Form/Type/SortableEntityType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Symbio\OrangeGate\AdminBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class SortableEntityType extends AbstractType
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'compound'          => false,
+        ));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getParent()
+    {
+        return 'entity';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getName()
+    {
+        return 'orangegate_type_sortable_entity';
+    }
+}
