4cfe630f1a754feb3b21f1c019725730134e46bd
Martin Cajthaml <martin.cajthaml@symbiodigital.com>
2015-11-18 09:29:29 +0100

    Removed setting locale

--- Listener/DoctrineExtensionListener.php
+++ Listener/DoctrineExtensionListener.php
@@ -18,16 +18,6 @@ class DoctrineExtensionListener implements ContainerAwareInterface
         $this->container = $container;
     }
 
-    public function onLateKernelRequest(GetResponseEvent $event)
-    {
-        $translatable = $this->container->get('gedmo.listener.translatable');
-        $sites = $this->container->get('orangegate.site.pool')->getSites();
-        if (!$sites || count($sites) == 0) {
-            throw new \Exception('No site available');
-        }
-        $translatable->setTranslatableLocale($sites[0]->getLocale());
-    }
-
     public function onKernelRequest(GetResponseEvent $event)
     {
         $securityContext = $this->container->get('security.context', ContainerInterface::NULL_ON_INVALID_REFERENCE);
